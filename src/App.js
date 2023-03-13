1a3d81067d2647a96472bb940d006af3ccfde787
dipbhujbal <dbhujbal91@gmail.com>
2023-03-13 22:36:26 +0530

    modified src

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>Newwwwww Third and FINAL Commit ....!!!</span>
+          <span style={{ color: 'red' }}>Newwwwww Commit ....!!!</span>
         </div>
       </header>
     </div>
